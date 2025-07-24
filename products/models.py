8862e205a5b9af98130811b276ddfaa3e8342378
shaun corcoran <shaun.corcoran@gmail.com>
2025-07-24 10:57:26 -0700

    tag component

--- products/models.py
+++ products/models.py
@@ -62,7 +62,6 @@ class ProductManager(models.Manager):
 
 
 
-
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     slug            = models.SlugField(blank=True, unique=True)
